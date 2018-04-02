1b5587872476c795b405eba16a57882890ff725a
Gung Jodi <jodipratama63@gmail.com>
2018-04-02 18:53:38 +0700

    First commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import MyComponent from './components/MyComponent'
+
+export default class App extends Component {
+  render() {
+    return (
+      <MyComponent title="CODING VLOG" />
+    );
+  }
+}
+
---
+++ components/MyComponent.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button
+} from 'react-native';
+
+export default class MyComponent extends Component {
+    constructor()
+    {
+        super();
+        this.state = {
+            content : "THIS IS A CONTENT",
+            input1 : 0,
+            input2: 0,
+            result:0
+        }
+    }
+
+    doAdd()
+    {
+        let result = parseInt(this.state.input1) + parseInt(this.state.input2);
+        this.setState({
+            result:result
+        });
+    }
+
+    doSubstract()
+    {
+        let result = parseInt(this.state.input1) - parseInt(this.state.input2);
+        this.setState({
+            result:result
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>{this.props.title}</Text>
+                <Text>{this.state.content}</Text>
+
+                <TextInput
+                    style={{width:200}}
+                    onChangeText={(text)=>this.setState({input1:text})}
+                    value={this.state.text}
+                    placeholder="Input 1"
+                />
+                <TextInput
+                    style={{width:200}}
+                    onChangeText={(text)=>this.setState({input2:text})}
+                    value={this.state.text}
+                    placeholder="Input 2"
+                />
+                <View style={styles.buttonView}>
+                    <Button title="ADD" onPress={()=>this.doAdd()}/>
+                    <Button title="SUBSTRACT" onPress={()=>this.doSubstract()}/>
+                </View>
+
+                <Text style={{fontSize:24}}>{this.state.result}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    buttonView:{
+        flexDirection: 'row',
+        justifyContent:'space-around',
+        width:200
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
